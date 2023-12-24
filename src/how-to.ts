d7d85a8c17ac79387f6f4393041b54024cd7c564
Sanusi Hassan <sanusihassan122@gmail.com>
2023-12-24 22:26:34 +0000

    added webschema and how to schemas

---
+++ src/how-to.ts
@@ -0,0 +1,270 @@
+// this is the final result i want, give me arabic version of this i.e translate it to arabic keep the property names as they are i.e preseve the schema of the object just translate the string values:
+export const OrganizePDFHOWTO = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "How to Organize PDF Pages",
+  description: "Step-by-step guide to organize pages in a PDF file.",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "Step 1: Upload PDF File",
+      text: "Upload the PDF file you want to organize.",
+      substeps: [
+        "Click on 'Select PDF files' button.",
+        "Select the PDF file from your device.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Step 2: View and Arrange Pages",
+      text: "View and arrange pages as desired.",
+      substeps: [
+        "Drag pages to rearrange their order.",
+        "Drop them in the desired sequence.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Step 3: Finalize Organization",
+      text: "Finalize the organization of PDF pages.",
+      substeps: [
+        "Review the page sequence.",
+        "Make any further adjustments if needed.",
+        "Ensure the desired order is set.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Step 4: Save Organized PDF",
+      text: "Download the organized PDF file.",
+      substeps: [
+        "Click 'Organized PDF'.",
+        "Wait for the process to finish.",
+        "Click 'Download' to get the modified PDF.",
+      ],
+    },
+  ],
+};
+
+export const OrganizePDFHOWTO_ar = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "كيفية تنظيم صفحات ملف PDF",
+  description: "دليل تدريجي لتنظيم الصفحات في ملف PDF.",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "الخطوة 1: تحميل ملف PDF",
+      text: "تحميل ملف PDF الذي ترغب في تنظيمه.",
+      substeps: ["انقر على زر 'تحديد ملفات PDF'.", "اختر ملف PDF من جهازك."],
+    },
+    {
+      "@type": "HowToStep",
+      name: "الخطوة 2: عرض وترتيب الصفحات",
+      text: "عرض وترتيب الصفحات حسب الرغبة.",
+      substeps: ["اسحب الصفحات لإعادة ترتيبها.", "أفلتها في التسلسل المطلوب."],
+    },
+    {
+      "@type": "HowToStep",
+      name: "الخطوة 3: إكمال التنظيم",
+      text: "إكمال تنظيم صفحات PDF.",
+      substeps: [
+        "مراجعة تسلسل الصفحات.",
+        "قم بأي تعديلات إضافية إذا لزم الأمر.",
+        "تأكد من تعيين الترتيب المطلوب.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "الخطوة 4: حفظ ملف PDF المنظم",
+      text: "تحميل ملف PDF المنظم.",
+      substeps: [
+        "انقر على 'ملف PDF المنظم'.",
+        "انتظر انتهاء العملية.",
+        "انقر على 'تحميل' للحصول على الملف PDF المعدل.",
+      ],
+    },
+  ],
+};
+
+export const OrganizePDFHOWTO_es = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "Cómo organizar páginas de un PDF",
+  description: "Guía paso a paso para organizar páginas en un archivo PDF.",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "Paso 1: Subir archivo PDF",
+      text: "Sube el archivo PDF que deseas organizar.",
+      substeps: [
+        "Haz clic en el botón 'Seleccionar archivos PDF'.",
+        "Selecciona el archivo PDF desde tu dispositivo.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Paso 2: Ver y organizar páginas",
+      text: "Visualiza y organiza las páginas según desees.",
+      substeps: [
+        "Arrastra las páginas para reorganizar su orden.",
+        "Suelta las páginas en la secuencia deseada.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Paso 3: Finalizar la organización",
+      text: "Finaliza la organización de las páginas del PDF.",
+      substeps: [
+        "Revisa la secuencia de páginas.",
+        "Realiza ajustes adicionales si es necesario.",
+        "Asegúrate de que el orden deseado esté establecido.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Paso 4: Guardar el PDF organizado",
+      text: "Descarga el archivo PDF organizado.",
+      substeps: [
+        "Haz clic en 'PDF organizado'.",
+        "Espera a que termine el proceso.",
+        "Haz clic en 'Descargar' para obtener el PDF modificado.",
+      ],
+    },
+  ],
+};
+
+export const OrganizePDFHOWTO_fr = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "Comment organiser les pages d'un PDF",
+  description:
+    "Guide étape par étape pour organiser les pages dans un fichier PDF.",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "Étape 1 : Télécharger un fichier PDF",
+      text: "Téléchargez le fichier PDF que vous souhaitez organiser.",
+      substeps: [
+        "Cliquez sur le bouton 'Sélectionner des fichiers PDF'.",
+        "Sélectionnez le fichier PDF depuis votre appareil.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Étape 2 : Visualiser et organiser les pages",
+      text: "Visualisez et organisez les pages selon vos souhaits.",
+      substeps: [
+        "Faites glisser les pages pour réorganiser leur ordre.",
+        "Déposez-les dans la séquence souhaitée.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Étape 3 : Finaliser l'organisation",
+      text: "Finalisez l'organisation des pages du PDF.",
+      substeps: [
+        "Vérifiez la séquence des pages.",
+        "Effectuez d'autres ajustements si nécessaire.",
+        "Assurez-vous que l'ordre désiré est défini.",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "Étape 4 : Sauvegarder le PDF organisé",
+      text: "Téléchargez le fichier PDF organisé.",
+      substeps: [
+        "Cliquez sur 'PDF organisé'.",
+        "Attendez la fin du processus.",
+        "Cliquez sur 'Télécharger' pour obtenir le PDF modifié.",
+      ],
+    },
+  ],
+};
+export const OrganizePDFHOWTO_hi = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "पीडीएफ पेजों को संगठित करने का तरीका",
+  description: "पीडीएफ फ़ाइल में पेजों को संगठित करने के लिए कदम-से-कदम गाइड।",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "स्टेप 1: पीडीएफ फ़ाइल अपलोड करें",
+      text: "वह पीडीएफ फ़ाइल अपलोड करें जिसे आप संगठित करना चाहते हैं।",
+      substeps: [
+        " 'सिलेक्ट पीडीएफ फ़ाइल्स' बटन पर क्लिक करें।",
+        "अपने डिवाइस से पीडीएफ फ़ाइल का चयन करें।",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "स्टेप 2: पेजों को देखें और व्यवस्थित करें",
+      text: "पेजों को देखें और वांछित रूप में व्यवस्थित करें।",
+      substeps: [
+        "पेजों को उनके क्रम को बदलने के लिए खींचें।",
+        "उन्हें इच्छित क्रम में छोड़ दें।",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "स्टेप 3: संगठन को पूरा करें",
+      text: "पीडीएफ पेजों का संगठन पूरा करें।",
+      substeps: [
+        "पेज क्रम की समीक्षा करें।",
+        "आवश्यक होने पर कोई अतिरिक्त समायोजन करें।",
+        "इच्छित क्रम सेट होने का सुनिश्चित करें।",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "स्टेप 4: संगठित पीडीएफ डाउनलोड करें",
+      text: "संगठित पीडीएफ फ़ाइल डाउनलोड करें।",
+      substeps: [
+        "'संगठित पीडीएफ' पर क्लिक करें।",
+        "प्रक्रिया को समाप्त होने तक प्रतीक्षा करें।",
+        "'डाउनलोड' पर क्लिक करें ताकि संशोधित पीडीएफ फ़ाइल प्राप्त की जा सके।",
+      ],
+    },
+  ],
+};
+
+export const OrganizePDFHOWTO_zh = {
+  "@context": "http://schema.org",
+  "@type": "HowTo",
+  name: "如何组织PDF页面",
+  description: "逐步指南，用于组织PDF文件中的页面。",
+  step: [
+    {
+      "@type": "HowToStep",
+      name: "步骤1：上传PDF文件",
+      text: "上传您想要组织的PDF文件。",
+      substeps: ["点击“选择PDF文件”按钮。", "从您的设备中选择PDF文件。"],
+    },
+    {
+      "@type": "HowToStep",
+      name: "步骤2：查看和排列页面",
+      text: "按需要查看和排列页面。",
+      substeps: ["拖动页面以重新排列其顺序。", "将它们放置在所需的顺序中。"],
+    },
+    {
+      "@type": "HowToStep",
+      name: "步骤3：完成组织",
+      text: "完成PDF页面的组织。",
+      substeps: [
+        "检查页面顺序。",
+        "如果需要，进行进一步调整。",
+        "确保设置了所需的顺序。",
+      ],
+    },
+    {
+      "@type": "HowToStep",
+      name: "步骤4：保存组织后的PDF",
+      text: "下载组织后的PDF文件。",
+      substeps: [
+        "点击“组织后的PDF”。",
+        "等待进程完成。",
+        "点击“下载”以获取修改后的PDF文件。",
+      ],
+    },
+  ],
+};
