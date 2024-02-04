682ddc9daf20df33de9d0ec17499c9fe682d09ef
root <root@vmi1587475.contaboserver.net>
2024-02-04 02:18:33 +0100

    prod ready

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withTM({
     includePaths: [path.join(__dirname, "node_modules")],
   },
   assetPrefix: isProd ? "/organize-pdf" : "",
-  output: "standalone",
+  output: "export",
   webpack: (config, { isServer }) => {
     // Only run this configuration on the client side
     if (!isServer) {
